1e177aeb1357d48252751fddab579272275d3ea6
Gowtham Nachu <gowthamnachu545@gmail.com>
2025-03-24 19:08:25 +0530

    Commiting

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Update CORS configuration for Render
 app.use(cors({
-  origin: ['https://esgyy6-3fagm6ngc-gowthamnachus-projects.vercel.app', 'http://localhost:3000'],
+  origin: ['esgyy.vercel.app', 'http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
